25c1b5622b9251e954044953c3d6ce3b03e4ba21
Wraken <florent.cucherousset@epitech.eu>
2018-01-10 16:57:02 +0100

    fix merge and push utils folder

--- Client/Graphics/main.cpp
+++ Client/Graphics/main.cpp
@@ -1,13 +1,13 @@
 
 #include "StarField.hpp"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
 
-  sf::Vector2u screenDimensions(1920, 1080);
+  sf::Vector2u screenDimensions(1024, 720);
   sf::RenderWindow window(
-      sf::VideoMode(screenDimensions.x, screenDimensions.y, 32),
-      "Game main window");
+      sf::VideoMode(screenDimensions.x, screenDimensions.y, 32), "R-TYPE");
 
   window.setSize(screenDimensions);
   window.setFramerateLimit(60);
@@ -26,14 +26,37 @@ int main() {
   starsSprite.setPosition(0, 0);
 
   Starfield Stars(screenDimensions.x, screenDimensions.y, 100);
+  sf::Font font;
+  font.loadFromFile("utils/zorque.ttf");
+  sf::Text text;
 
+  text.setFont(font);
+  text.setString("R-TYPE");
+  text.setCharacterSize(84);
+  text.setColor(sf::Color::White);
+  text.setStyle(sf::Text::Bold);
+
+  sf::FloatRect t = text.getLocalBounds();
+  text.setOrigin(t.left + t.width / 2.0f, t.top + t.height / 2.0f);
+
+  text.setPosition(
+      sf::Vector2f(screenDimensions.x / 2.0f, screenDimensions.y / 2.0f - 200));
+
+  sf::Texture button;
+  sf::Sprite SButton;
+  button.loadFromFile("utils/Button.png");
+  SButton.setTexture(button);
+  SButton.setPosition(screenDimensions.x / 2, screenDimensions.y / 2);
   // Game loop
   while (window.isOpen()) {
     sf::Event event;
 
     while (window.pollEvent(event)) {
-
       switch (event.type) {
+      case sf::Event::MouseButtonPressed:
+        if (event.mouseButton.button == sf::Mouse::Left)
+          std::cout << "DROIT" << '\n';
+        break;
       case sf::Event::Closed:
         window.close();
         break;
@@ -56,6 +79,8 @@ int main() {
     Stars.DrawStar(starsTexture);
     window.clear(sf::Color(0, 0, 0));
     window.draw(starsSprite);
+    window.draw(text);
+    window.draw(SButton);
     window.display();
   }
 
