9ed7468bae42b11b632dcac4dc87cd172db6dd42
Victorien Fischer <victorien.fischer@epitech.eu>
2018-01-10 17:24:06 +0100

    fixed iterator omg

--- Client/Graphics/StarField.cpp
+++ Client/Graphics/StarField.cpp
@@ -15,17 +15,19 @@ Starfield::Starfield(int xRes, int yRes, unsigned int nbStars) {
 }
 
 void Starfield::updateStar() {
-    for (auto it = _stars.begin(); it != _stars.end(); it++) {
-        if (it->getY() >= _yRes) {
-            _stars.erase(it);
-        }
-    }
-    for (auto it = _stars.begin(); it != _stars.end(); it++) {
-        it->moveY(it->getSize());
-    }
-    while (_stars.size() <= _nbStars) {
-      _stars.push_back(Star(_rx(_rng), _ry(_rng), _rsize(_rng)));
-    }
+	std::vector<Star>::iterator it = _stars.begin();
+	while (it != _stars.end()) {
+		if (it->getY() >= _yRes)
+			it = _stars.erase(it);
+		else
+			it++;
+	}
+	for (it = _stars.begin(); it != _stars.end(); it++) {
+		it->moveY(it->getSize());
+	}
+	while (_stars.size() <= _nbStars) {
+		_stars.push_back(Star(_rx(_rng), _ry(_rng), _rsize(_rng)));
+	}
 }
 
 void Starfield::drawStar(sf::Texture &t) const {
