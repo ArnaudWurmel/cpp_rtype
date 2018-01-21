776fa7aba8a3acb98205c0631c93cc1572e9870c
Arnaud WURMEL <wurmel_a@arnauds-mbp.home>
2018-01-21 22:35:57 +0100

    libray

--- ServerGame/Monster/BasicMonster/mandatory.cpp
+++ ServerGame/Monster/BasicMonster/mandatory.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <memory>
-#include "BossMonster.hh"
+#include "BasicMonster.hh"
 
+#ifdef _WIN32
 #define DllExport   __declspec( dllexport )   
 
 extern "C" DllExport rtp::AEnemy  *instanciateMonster(unsigned int, int, int);
@@ -9,3 +10,14 @@ extern "C" DllExport rtp::AEnemy  *instanciateMonster(unsigned int, int, int);
 DllExport rtp::AEnemy *instanciateMonster(unsigned int id, int x, int y) {
     return new Monster::BasicMonster(id, x, y);
 }
+
+#else
+extern "C" rtp::AEnemy  *instanciateMonster(unsigned int, int, int);
+
+rtp::AEnemy *instanciateMonster(unsigned int id, int x, int y) {
+    return new Monster::BasicMonster(id, x, y);
+}
+
+#endif
+
+
--- ServerGame/Monster/BossMonster/mandatory.cpp
+++ ServerGame/Monster/BossMonster/mandatory.cpp
@@ -2,10 +2,20 @@
 #include <memory>
 #include "BossMonster.hh"
 
-#define DllExport   __declspec( dllexport )   
+#ifdef _WIN32
+#define DllExport   __declspec( dllexport )
 
 extern "C" DllExport rtp::AEnemy  *instanciateMonster(unsigned int, int, int);
 
 DllExport rtp::AEnemy *instanciateMonster(unsigned int id, int x, int y) {
     return new Monster::BossMonster(id, x, y);
 }
+
+#else
+extern "C" rtp::AEnemy  *instanciateMonster(unsigned int, int, int);
+
+rtp::AEnemy *instanciateMonster(unsigned int id, int x, int y) {
+    return new Monster::BossMonster(id, x, y);
+}
+
+#endif
