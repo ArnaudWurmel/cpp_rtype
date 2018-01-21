1b0aea91f90d629c2696cc7673849cd1812c89a5
Victorien Fischer <victorien.fischer@epitech.eu>
2018-01-21 19:16:07 +0100

    trying out cmake symlinks

---
+++ ServerAuth/symlinks.bat
@@ -0,0 +1,3 @@
+mklink /D Logger ..\Common\Logger
+mklink /D NetworkAbstract ..\Common\NetworkAbstract
+mklink /D ArgumentLoader ..\Common\ArgumentLoader
---
+++ ServerAuth/symlinks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ln -ls ..\Common\Logger Logger
+ln -ls ..\Common\NetworkAbstract NetworkAbstract
+ln -ls ..\Common\ArgumentLoader ArgumentLoader
