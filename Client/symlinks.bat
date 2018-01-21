c46f909559ec7c8bf19ced221fa8e54bf18da750
Victorien Fischer <victorien.fischer@epitech.eu>
2018-01-21 21:03:58 +0100

    Client symlinks cmake

---
+++ Client/symlinks.bat
@@ -0,0 +1,4 @@
+mklink /D Logger ..\Common\Logger
+mklink /D NetworkAbstract ..\Common\NetworkAbstract
+mklink /D ArgumentLoader ..\Common\ArgumentLoader
+mklink /D Exceptions ..\Common\Exceptions
---
+++ Client/symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -s ../Common/Logger Logger
+ln -s ../Common/NetworkAbstract NetworkAbstract
+ln -s ../Common/ArgumentLoader ArgumentLoader
+ln -s ../Common/Exceptions Exceptions
